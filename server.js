55770e52e7cd6583a170040d33a3d0b0016b6b1c
MrTGK0386 <71849240+MrTGK0386@users.noreply.github.com>
2024-06-10 22:54:20 +0200

    Comment done on server.js

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const http = require('http');
-const express = require('express');
-const session = require('express-session');
-const { Server } = require('socket.io');
-const port = 34090;
-const sequelize = require("./database.js");
-const passport = require("passport");
+const port = 34090; // port de l'application
 
-const app = express();
+const http = require('http'); //librairie de protocole http et de création de serveur web
+const express = require('express'); //librairie d'appli web
+const session = require('express-session'); // extension d'express pour la gestion de session (comme en php)
+const { Server } = require('socket.io'); // librairie de gestion de socket
+const sequelize = require("./database.js"); // Gestionnaire et intégrateur de base de donnée
+const passport = require("passport"); // Gestionnaire d'authentification
 
-const indexRouter = require("./routes/index");
-const authRouter = require("./routes/auth");
+const app = express(); // Instanciation de l'application
 
-sequelize.sync().then(()=>{
+const indexRouter = require("./routes/index"); // Paramétrage du routeur par défaut
+const authRouter = require("./routes/auth"); // Paramétrage du routeur d'authentification
+
+sequelize.sync().then(()=>{ // Synchronisation à la base de donnée
     console.log("database up !.");
 });
 
 
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: "SECRETKEY", resave: true, saveUninitialized: true }));
+app.use(express.static('public')); //Définition d'un dossier "public" qui contient les données lié au site (images ,fonts, css)
+app.set('view engine', 'ejs'); // Application du modèle de vue EJS
+app.use(express.urlencoded({ extended: true })); // Permet de récupérer les objets des requêtes post et put sous forme de string ou d'arrays
+app.use(session({ secret: "SECRETKEY", resave: true, saveUninitialized: true })); // Définis une clé de chiffrement pour les session express (pas top niveau sécu)
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session()) //Génère une session Express avec l'authentification de passport
 
-app.use('/', indexRouter)
-app.use('/auth', authRouter);
+app.use('/', indexRouter); app.use('/auth', authRouter); // Indique les routeur à utilisé en fonction de l'URL de la requête
 
-const server = http.createServer(app);
-const io = new Server(server);
+const server = http.createServer(app); //Génère un serveur web qui héberge une appli express
+const io = new Server(server); //créer un socket pour le serveur
 
-var buttonValue = null;
+var buttonValue = null; // Valeur d'un bouton pour faire des test sur les socket
 
-io.on('connection', function(socket) {
+io.on('connection', function(socket) { //gestion des événement suite à une connexion avec un socket client
     console.log("New user got a socket connection");
     socket.on('addOne', function() {
         buttonValue++;
         io.emit('addOneResponse', buttonValue);
     })
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function() { // gère la déconnection
         console.log("A socket was disbanded");
     });
 });
 
 
-server.listen(port, () => {
+server.listen(port, () => { // Ouverture du serveur sur le port défini au début
     console.log(`Server started at http://localhost:${port}`);
 });
