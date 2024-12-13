6aa8f2e772371160e76ab3a450d0be317c12d933
MrTGK0386 <71849240+MrTGK0386@users.noreply.github.com>
2024-12-13 13:57:29 +0100

    ADD : Ajout script suppression user (marche pas)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,36 @@ const socket = io();
 
 const buttonVal = document.getElementById("buttonVal");
 
+function redirectByPost(url, parameters, inNewTab) {
+    parameters = parameters || {};
+    inNewTab = inNewTab === undefined ? true : inNewTab;
+
+    var form = document.createElement("form");
+    form.id = "reg-form";
+    form.name = "reg-form";
+    form.action = url;
+    form.method = "post";
+    form.enctype = "multipart/form-data";
+
+    if (inNewTab) {
+        form.target = "_blank";
+    }
+
+    Object.keys(parameters).forEach(function (key) {
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = key;
+        input.value = parameters[key];
+        form.appendChild(input);
+    });
+
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+
+    return false;
+}
+
 buttonVal.addEventListener("click", function() {
     socket.emit("addOne");
     console.log("addOne, done");
@@ -12,3 +42,7 @@ socket.on("addOneResponse", (buttonValue) => {
     const valeur = document.getElementById("valeur");
     valeur.innerHTML = `Valeur: ${buttonValue}`;
 });
+
+function deleteUser(userid){
+    redirectByPost('/action/deleteUser?id=' + userid,null,false);
+}
