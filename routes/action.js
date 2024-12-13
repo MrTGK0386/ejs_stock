4e39f72b98618ee8bffdb20a44146c1e594833fd
MrTGK0386 <71849240+MrTGK0386@users.noreply.github.com>
2024-12-13 15:20:19 +0100

    MOD : refonte logique de la route + ajout middleware isAdmin isAuth

--- routes/action.js
+++ routes/action.js
@@ -4,22 +4,36 @@ const Stock = require('../models/').mfgs_stock;
 const User = require('../models').mfgs_users;
 const {where} = require("sequelize");
 
-router.post('/deleteUser', async (req, res) => {
-    if (req.user) {
-        const admin = req.user.admin;
-        const userid = req.params.userid;
-        console.log(userid,req.user);
-        const userToDelete = User.findByPk(userid);
+function isAuthenticated(req, res, next) {
+    if (!req.user) {
+        return res.status(401).render('unauthorized');
+    }
+    next();
+}
 
-        console.log(userToDelete+'Post OK')
+function isAdmin(req, res, next) {
+    if (!req.user.admin) {
+        return res.status(403).render('unauthorized');
+    }
+    next();
+}
 
-        if (admin === 1 || userid) {
 
-            await userToDelete.destroy();
 
-        } else {
-            res.render('unauthorized', {});
+router.post('/deleteUser', isAdmin, isAuthenticated, async (req, res) => {
+    try {
+        const userid = req.params.id;
+
+        const userToDelete = await User.findByPk(userid);
+        if (!userToDelete) {
+            return res.status(404).render('404', {pagetitle: 'Erreur 404'});
         }
+
+        await userToDelete.destroy();
+        return res.status(200).redirect('/dashboard/admin');
+    } catch (error) {
+        console.error(`Erreur lors de la suppression de l'utilisateur`,error);
+        return res.status(500).render('error',{pagetitle:'Erreur 500',error:'500'});
     }
 })
 
